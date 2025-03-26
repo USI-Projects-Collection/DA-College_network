3defd510f7afe30801edc5a59faa63efb12ca557
paolodeidda <paolodeidda.97@gmail.com>
2025-03-26 18:03:43 +0100

    updated gitignore

---
+++ source.py
@@ -0,0 +1,24 @@
+
+import numpy as np
+# %%
+print("gay")
+
+print("ciao")
+# %%
+print("omofobo")
+
+# %%
+a = np.random.rand(10)
+print(a)
+# %%
+
+# print test image
+import matplotlib.pyplot as plt
+import numpy as np
+
+img = np.random.rand(100, 100)
+plt.imshow(img)
+plt.show()
+
+# %%
+
